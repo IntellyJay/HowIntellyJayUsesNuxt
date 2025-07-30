82b58ea6535a33a84adb2b840adb67a3d5d97838
IntellyJay <intellyjay@gmail.com>
2025-07-30 14:12:07 +0900

    Created: layouts/default.vue, error.vue

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            defaultValues: {
+                pageName: ''
+            }
+        }
+    },
+    watch: {
+        '$route.fullPath': function() {
+            this.defaultValues.pageName = this.$route.fullPath;
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
